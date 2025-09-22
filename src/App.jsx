523e3f657b95dd4c1122b4143368c3ecfb115840
gabe-1byte <gabe.m.buckman@gmail.com>
2025-09-22 12:51:06 -0600

    CF 4.8 Work
    Add Alerts:
    - InfoAlert: no results found for entered city
    - ErrorAlert: only valid numbers allowed
    
    Updated App.jsx to display the alerts
    
    Updated App.css to properly display alerts
    
    Edited tests to work with added alerts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 
 import './App.css';
 
@@ -11,6 +12,8 @@ function App() {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [events, setEvents] = useState([]);
   const [currentCity, setCurrentCity] = useState('See all cities');
+  const [infoAlert , setInfoAlert] = useState("");
+  const [errorAlert , setErrorAlert] = useState("");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -33,8 +36,18 @@ function App() {
 
   return (
     <div className="App">
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents onNumberChanged={setCurrentNOE} />
+      <div className="alert-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+      </div>
+      <CitySearch 
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert}
+      />
+      <NumberOfEvents onNumberChanged={setCurrentNOE} 
+        setErrorAlert={setErrorAlert}
+      />
       <EventList events={events} />
     </div>
   );
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -4,20 +4,20 @@ import userEvent from "@testing-library/user-event";
 import NumberOfEvents from "../components/NumberOfEvents";
 
 test('NumberOfEvents component contains a textbox', () => {
-    const { getByRole } = render(<NumberOfEvents />);
+    const { getByRole } = render(<NumberOfEvents setErrorAlert={() => {}} />);
     const textbox = getByRole('textbox');
     expect(textbox).toBeInTheDocument();
 });
 
 test('NumberOfEvents component has a default value of 32', () => {
-    const { getByRole } = render(<NumberOfEvents />);
+    const { getByRole } = render(<NumberOfEvents setErrorAlert={() => {}} />);
     const textbox = getByRole('textbox');
     expect(textbox.value).toBe('32');
 });
 
 test('NumberOfEvents component updates value when user types', async () => {
     const user = userEvent.setup();
-    const { getByRole } = render(<NumberOfEvents />);
+    const { getByRole } = render(<NumberOfEvents setErrorAlert={() => {}} />);
     const textbox = getByRole('textbox');
     await user.type(textbox, '{backspace}{backspace}10');
     expect(textbox.value).toBe('10');
